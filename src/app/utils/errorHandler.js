663fd3d41977adf33c26220cce3670f8a56a8ddc
horusen <babacarkane@meltwater.org>
2023-05-22 06:32:33 +0000

    fix: error handler

--- src/app/utils/errorHandler.js
+++ src/app/utils/errorHandler.js
@@ -1,8 +1,15 @@
 // This middleware is used to handle errors in the application.
 // It is used in the main app.js file.
 module.exports = function (err, req, res, next) {
+	if (Object.keys(err).length === 0) {
+		err = { message: "Internal server error", code: 500 };
+	}
+
 	const response = { message: err.message };
-	if (Object.keys(err.errors).length > 0) {
+
+	const errors = err?.errors || {};
+
+	if (Object.keys(errors).length > 0) {
 		response.errors = err.errors;
 	}
 	res.status(err.code).send(response);
